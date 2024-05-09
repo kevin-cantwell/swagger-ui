5b4564fa93a6c4c6deaaa0c499b217d60174effd
Kevin Cantwell <kevin.cantwell@gmail.com>
2024-05-09 17:01:58 -0400

    allow url from query param

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -1,9 +1,16 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
+  const urlParams = new URLSearchParams(window.location.search);
+  let url = urlParams.get('url')
+  if (!url) {
+    // default url
+    url = "https://petstore.swagger.io/v2/swagger.json";
+  }
+
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: url,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
